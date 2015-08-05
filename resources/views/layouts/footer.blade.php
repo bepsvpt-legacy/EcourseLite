df047d1a5d304c7f631c4ba6529c11376db33921
BePsvPT <freedom@csie.io>
2015-08-05 14:53:44 +0800

    update additional files

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="center-align">
+        <span class="title">Created by: Freedom, Collaborator: <a href="https://www.facebook.com/profile.php?id=1281974381" target="_blank">Gary Gong</a></span><br>
+        <span>bepsvpt.net © 2015</span>
+    </div>
+</footer>
